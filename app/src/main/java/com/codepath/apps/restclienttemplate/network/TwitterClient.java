20a00ed0a04408a7bd4e5469608cd6cb0661e03e
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 12:02:38 +0530

    added rest url , consumer_key , consumer_secret etc

--- app/src/main/java/com/codepath/apps/restclienttemplate/network/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/network/TwitterClient.java
@@ -1,7 +1,24 @@
 package com.codepath.apps.restclienttemplate.network;
 
+import android.content.Context;
+
+import com.codepath.oauth.OAuthBaseClient;
+
+import org.scribe.builder.api.Api;
+import org.scribe.builder.api.TwitterApi;
+
 /**
  * Created by ankit on 3/2/16.
  */
-public class TwitterClient {
+public class TwitterClient extends OAuthBaseClient {
+
+    public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class;
+    public static final String REST_URL = "https://api.twitter.com/1.1/";
+    public static final String REST_CONSUMER_KEY = "CRPpOP2x2ALbUNC77M1tgslvG";
+    public static final String REST_CONSUMER_SECRET = "1N5SGMm1uMwFJXAyOEib7xSqzDK6XyKZLaCx0T2QIjIFkd8Smb";
+    public static final String REST_CALLBACK_URL = "oauth://cpsimpletweets";
+
+    public TwitterClient(Context context) {
+        super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
+    }
 }
