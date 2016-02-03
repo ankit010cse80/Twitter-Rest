330e51bc5877c931b7ab148a27ffbd30efd737a1
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 16:53:27 +0530

    user info

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,7 +1,34 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+import java.io.Serializable;
+
 /**
  * Created by ankit on 3/2/16.
  */
-public class User {
+
+@Table(name = "User")
+public class User extends Model implements Serializable {
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "u_id", unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
+    private long uid;
+    @Column(name = "screen_name")
+    private String screenName;
+    @Column(name = "profile_image_url")
+    private String profileImageURL;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "follower_count")
+    private long followerCount;
+    @Column(name = "following_count")
+    private long followingCount;
+    @Column(name = "tweet_count")
+    private long tweetCount;
+    @Column(name = "background_profile_image_url")
+    private String backgroundProfileImageURL;
 }
