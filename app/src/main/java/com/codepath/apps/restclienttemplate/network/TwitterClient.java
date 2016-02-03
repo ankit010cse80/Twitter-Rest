e8ee9b2d5abd886ef31e11205ecb2d3d75589165
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 12:48:22 +0530

    added new method getHomeTimeline

--- app/src/main/java/com/codepath/apps/restclienttemplate/network/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/network/TwitterClient.java
@@ -3,8 +3,11 @@ package com.codepath.apps.restclienttemplate.network;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.widget.Toast;
 
 import com.codepath.oauth.OAuthBaseClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import org.scribe.builder.api.Api;
 import org.scribe.builder.api.TwitterApi;
@@ -23,6 +26,27 @@ public class TwitterClient extends OAuthBaseClient {
     public TwitterClient(Context context) {
         super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
     }
+
+    public void getHomeTimeline(long sinceId, long maxID, AsyncHttpResponseHandler handler) {
+        if (isNetworkAvailable()) {
+            // Get API
+            String apiUrl = getApiUrl("statuses/home_timeline.json");
+            // Create parameter list
+            RequestParams params = new RequestParams();
+            params.put("count", 50);
+            params.put("since_id", 1);
+
+            if (maxID != Long.MAX_VALUE) {
+                params.put("max_id", maxID);
+            }
+
+            // Execute the request
+            getClient().get(apiUrl, params, handler);
+        } else {
+            Toast.makeText(context, "Network not available. Please check your network connection", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 
     // Check if Internet is available
