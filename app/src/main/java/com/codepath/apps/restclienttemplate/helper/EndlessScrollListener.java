3c70bbcdecd9b16bdae625faf63986e99ece9b44
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 18:51:06 +0530

    added variables

--- app/src/main/java/com/codepath/apps/restclienttemplate/helper/EndlessScrollListener.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/helper/EndlessScrollListener.java
@@ -1,7 +1,36 @@
 package com.codepath.apps.restclienttemplate.helper;
 
+import android.widget.AbsListView;
+
 /**
  * Created by ankit on 3/2/16.
  */
-public class EndlessScrollListener {
+public class EndlessScrollListener implements AbsListView.OnScrollListener {
+
+
+    // The minimum amount of items to have below your current scroll position
+    // before loading more.
+    private int visibleThreshold = 5;
+    // The current offset index of data you have loaded
+    private int currentPage = 0;
+    // The total number of items in the dataset after the last load
+    private int previousTotalItemCount = 0;
+    // True if we are still waiting for the last set of data to load.
+    private boolean loading = true;
+    // Sets the starting page index
+    private int startingPageIndex = 0;
+
+    public EndlessScrollListener() {
+    }
+
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+    }
 }
