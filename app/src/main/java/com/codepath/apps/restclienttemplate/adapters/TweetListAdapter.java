46b8dee820f9432b963fa6ce7406ffb19ca89220
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 23:05:22 +0530

    added ViewHolder class

--- app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListAdapter.java
@@ -2,6 +2,9 @@ package com.codepath.apps.restclienttemplate.adapters;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
@@ -9,6 +12,18 @@ import com.codepath.apps.restclienttemplate.models.Tweet;
  * Created by ankit on 3/2/16.
  */
 public class TweetListAdapter extends ArrayAdapter<Tweet> {
+
+    private static class ViewHolder {
+        ImageView ivProfileImage;
+        TextView tvUsername;
+        TextView tvTweetBody;
+        TextView tvTimeStamp;
+        TextView tvRetweet;
+        TextView tvFavorites;
+        ImageButton btnReply;
+    }
+
+
     public TweetListAdapter(Context context, int resource) {
         super(context, resource);
     }
