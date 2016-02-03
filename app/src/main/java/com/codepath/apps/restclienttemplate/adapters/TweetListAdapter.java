a8b6bd475fe11bc4c272988fd684215e658cb549
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 23:11:02 +0530

    modified TweetListAdapter and created getView method

--- app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListAdapter.java
@@ -1,12 +1,21 @@
 package com.codepath.apps.restclienttemplate.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.codepath.apps.restclienttemplate.TwitterApplication;
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 /**
  * Created by ankit on 3/2/16.
@@ -23,8 +32,14 @@ public class TweetListAdapter extends ArrayAdapter<Tweet> {
         ImageButton btnReply;
     }
 
+    private TwitterClient client = TwitterApplication.getRestClient();
 
-    public TweetListAdapter(Context context, int resource) {
-        super(context, resource);
+    public TweetListAdapter(Context context, List<Tweet> tweets) {
+        super(context, android.R.layout.simple_list_item_1, tweets);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
     }
 }
