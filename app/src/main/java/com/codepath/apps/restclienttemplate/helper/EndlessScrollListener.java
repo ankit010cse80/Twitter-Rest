0f2d1366488632b6563b63450ad3ca4fe4fd8d16
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 19:00:57 +0530

    added method to listen for scroll and then populate data

--- app/src/main/java/com/codepath/apps/restclienttemplate/helper/EndlessScrollListener.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/helper/EndlessScrollListener.java
@@ -5,7 +5,7 @@ import android.widget.AbsListView;
 /**
  * Created by ankit on 3/2/16.
  */
-public class EndlessScrollListener implements AbsListView.OnScrollListener {
+public abstract class EndlessScrollListener implements AbsListView.OnScrollListener {
 
 
     // The minimum amount of items to have below your current scroll position
@@ -39,8 +39,35 @@ public class EndlessScrollListener implements AbsListView.OnScrollListener {
 
     }
 
-    @Override
-    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
 
+    // Defines the process for actually loading more data based on page
+    public abstract void onLoadMore(int page, int totalItemsCount);
+
+    @Override
+    public void onScroll(AbsListView view,int firstVisibleItem,int visibleItemCount,int totalItemCount)
+    {
+        // If the total item count is zero and the previous isn't, assume the
+        // list is invalidated and should be reset back to initial state
+        if (totalItemCount < previousTotalItemCount) {
+            this.currentPage = this.startingPageIndex;
+            this.previousTotalItemCount = totalItemCount;
+            if (totalItemCount == 0) { this.loading = true; }
+        }
+        // If it’s still loading, we check to see if the dataset count has
+        // changed, if so we conclude it has finished loading and update the current page
+        // number and total item count.
+        if (loading && (totalItemCount > previousTotalItemCount)) {
+            loading = false;
+            previousTotalItemCount = totalItemCount;
+            currentPage++;
+        }
+
+        // If it isn’t currently loading, we check to see if we have breached
+        // the visibleThreshold and need to reload more data.
+        // If we do need to reload some more data, we execute onLoadMore to fetch the data.
+        if (!loading && (totalItemCount - visibleItemCount)<=(firstVisibleItem + visibleThreshold)) {
+            onLoadMore(currentPage + 1, totalItemCount);
+            loading = true;
+        }
     }
 }
