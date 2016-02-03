4ee957e38a490c0287f805cf9529ea9064daca4e
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 16:34:42 +0530

    added static variables

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,7 +1,22 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Table;
+import com.codepath.apps.restclienttemplate.TwitterApplication;
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
+
+import java.io.Serializable;
+
 /**
  * Created by ankit on 3/2/16.
  */
-public class Tweet {
+
+@Table(name = "Tweets")
+public class Tweet extends Model implements Serializable {
+
+    private static long maxId = Long.MAX_VALUE;
+    private static long sinceId = 1;
+    private static TwitterClient client = TwitterApplication.getRestClient();
+
+
 }
