838e631eaee2299f15eea5ed60b273c502b72a37
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 00:05:16 +0530

    added menu and method to handle menu items

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ComposeTweetActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ComposeTweetActivity.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -31,4 +32,28 @@ public class ComposeTweetActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose_tweet);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_compose_tweet, menu);
+        // Get menu item
+        menuItem = menu.findItem(R.id.miCharCount);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
