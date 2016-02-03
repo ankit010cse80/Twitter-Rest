6c3439e4570dae5fce6f8f5c620403dca889a439
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 23:03:00 +0530

    dummy icons

--- app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListAdapter.java
@@ -1,7 +1,15 @@
 package com.codepath.apps.restclienttemplate.adapters;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
 /**
  * Created by ankit on 3/2/16.
  */
-public class TweetListAdapter {
+public class TweetListAdapter extends ArrayAdapter<Tweet> {
+    public TweetListAdapter(Context context, int resource) {
+        super(context, resource);
+    }
 }
