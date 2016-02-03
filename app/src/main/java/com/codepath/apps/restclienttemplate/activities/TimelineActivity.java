0a409755d47c3a6629c730c28c1dbccf58de1992
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 13:23:10 +0530

    added onCreate method

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
@@ -1,7 +1,24 @@
 package com.codepath.apps.restclienttemplate.activities;
 
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+
+import com.codepath.apps.restclienttemplate.R;
+
 /**
  * Created by ankit on 3/2/16.
  */
-public class TimelineActivity {
+public class TimelineActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+        // Get view pager
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
+
+    }
 }
