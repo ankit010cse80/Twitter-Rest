e3fa68365683e371d845caf3e53e625f24e91b7c
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 23:13:30 +0530

    getting all the view layout

--- app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListAdapter.java
@@ -1,7 +1,6 @@
 package com.codepath.apps.restclienttemplate.adapters;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +9,10 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.TwitterApplication;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.apps.restclienttemplate.network.TwitterClient;
-import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -41,5 +40,25 @@ public class TweetListAdapter extends ArrayAdapter<Tweet> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        // Get tweet
+        final Tweet tweet = getItem(position);
+        //View look up cache stored in tag;
+        ViewHolder viewHolder = null;
+        if (convertView == null) {      //If recycled view is not available
+            viewHolder = new ViewHolder();
+            // create new convert view
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
+            // Find subviews
+            viewHolder.ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
+            viewHolder.tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
+            viewHolder.tvTweetBody = (TextView) convertView.findViewById(R.id.tvBody);
+            viewHolder.tvTimeStamp = (TextView) convertView.findViewById(R.id.tvTimeStamp);
+            viewHolder.tvRetweet = (TextView) convertView.findViewById(R.id.tvRetweet);
+            viewHolder.tvFavorites = (TextView) convertView.findViewById(R.id.tvFavorites);
+            viewHolder.btnReply = (ImageButton) convertView.findViewById(R.id.btnReply);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
     }
 }
