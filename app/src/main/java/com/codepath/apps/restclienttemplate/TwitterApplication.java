35bc66fd6a37008213600a54b0140b125451ab64
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-03 12:29:46 +0530

    added new method getRestClient

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterApplication.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterApplication.java
@@ -1,7 +1,24 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Context;
+
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
+
 /**
  * Created by ankit on 3/2/16.
  */
-public class TwitterApplication {
+public class TwitterApplication extends com.activeandroid.app.Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TwitterApplication.context = this;
+    }
+
+    public static TwitterClient getRestClient() {
+        return (TwitterClient) TwitterClient.getInstance(TwitterClient.class, TwitterApplication.context);
+    }
+
 }
