b674d139bf7037bf7dd28fd0d8e67b1f3e0ae8d7
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 19:31:50 +0530

    added onCreateOptionsMenu

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
@@ -3,6 +3,7 @@ package com.codepath.apps.restclienttemplate.activities;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 
 import com.codepath.apps.restclienttemplate.R;
 
@@ -20,5 +21,13 @@ public class TimelineActivity extends ActionBarActivity {
         // Get view pager
         ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_timeline, menu);
+        return true;
     }
 }
