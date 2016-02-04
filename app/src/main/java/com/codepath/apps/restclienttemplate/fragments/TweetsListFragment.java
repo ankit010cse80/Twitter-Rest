53be553e870defb33471cc979f0e6c82154c03b4
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 16:56:01 +0530

    setted random color

--- app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
@@ -83,6 +83,13 @@ public abstract class TweetsListFragment extends Fragment {
             }
         });
 
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
         return v;
     }
 
