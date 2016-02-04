70174f1b97ea3895c301829df91360fd89ac3e34
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 13:30:46 +0530

    added skeleton methods like onCreate , onCreateOptionsMenu etc

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
@@ -1,9 +1,13 @@
 package com.codepath.apps.restclienttemplate.activities;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.models.User;
 import com.codepath.apps.restclienttemplate.network.TwitterClient;
 
@@ -21,4 +25,34 @@ public class ProfileActivity extends ActionBarActivity {
     private TextView tvFollowingCount;
     private TextView tvFollowerCount;
     private ImageView ivBackgroundImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
