256322664bc9d5f54c6e09a3c008e7ae718cf37d
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 15:14:37 +0530

    declaring variables and adapter class

--- app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
@@ -1,9 +1,21 @@
 package com.codepath.apps.restclienttemplate.fragments;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.widget.ListView;
+
+import com.codepath.apps.restclienttemplate.adapters.TweetListAdapter;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.ArrayList;
 
 /**
  * Created by ankit on 3/2/16.
  */
 public class TweetsListFragment extends Fragment {
+
+    private SwipeRefreshLayout swipeContainer;
+    private TweetListAdapter tweetAdapter;
+    private ListView lvTweets;
+    private ArrayList<Tweet> tweets;
 }
