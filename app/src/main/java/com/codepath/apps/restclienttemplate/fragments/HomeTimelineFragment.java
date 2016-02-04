a08509cebf0f06617af78a8197c82cdd9cf9236e
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 22:11:24 +0530

    added onCreate method

--- app/src/main/java/com/codepath/apps/restclienttemplate/fragments/HomeTimelineFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/HomeTimelineFragment.java
@@ -1,9 +1,34 @@
 package com.codepath.apps.restclienttemplate.fragments;
 
+import android.os.Bundle;
+
+import com.codepath.apps.restclienttemplate.TwitterApplication;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
+
+import java.util.ArrayList;
+
 /**
  * Created by ankit on 3/2/16.
  */
 public class HomeTimelineFragment extends TweetsListFragment {
+
+    private TwitterClient client;
+    private Boolean shouldRefresh = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the client
+        client = TwitterApplication.getRestClient(); // singleton client
+        ArrayList<Tweet> tweetList = new ArrayList<>();
+        tweetList.addAll(Tweet.getTweetsFromDB());
+
+        // Add to the adapter
+        addAll(tweetList);
+
+    }
+
     @Override
     public void populateTimelineWithMaxId(Long sinceId, long maxId) {
 
