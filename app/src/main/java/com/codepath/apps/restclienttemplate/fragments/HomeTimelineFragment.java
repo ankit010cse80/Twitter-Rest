9fe8274802225ce2ef543048999aabb68a5063c9
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 22:07:18 +0530

    extending TweetListFragment

--- app/src/main/java/com/codepath/apps/restclienttemplate/fragments/HomeTimelineFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/HomeTimelineFragment.java
@@ -3,5 +3,9 @@ package com.codepath.apps.restclienttemplate.fragments;
 /**
  * Created by ankit on 3/2/16.
  */
-public class HomeTimelineFragment {
+public class HomeTimelineFragment extends TweetsListFragment {
+    @Override
+    public void populateTimelineWithMaxId(Long sinceId, long maxId) {
+
+    }
 }
