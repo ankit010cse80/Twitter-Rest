1ec9c573cab0d40869f5a392a2212eabe6f89472
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 22:16:10 +0530

    calling getHomeTimeLine in populateTimelineWithMaxId method

--- app/src/main/java/com/codepath/apps/restclienttemplate/fragments/HomeTimelineFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/HomeTimelineFragment.java
@@ -1,10 +1,16 @@
 package com.codepath.apps.restclienttemplate.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.codepath.apps.restclienttemplate.TwitterApplication;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.apps.restclienttemplate.network.TwitterClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -30,7 +36,40 @@ public class HomeTimelineFragment extends TweetsListFragment {
     }
 
     @Override
-    public void populateTimelineWithMaxId(Long sinceId, long maxId) {
+    public void populateTimelineWithMaxId(Long sinceId, final long maxId) {
 
+        if (maxId == Long.MAX_VALUE) {
+            Tweet.setMaxId(Long.MAX_VALUE);
+        }
+
+        long since_id = 1;
+
+        if (sinceId != null) {
+            since_id = sinceId;
+        }
+
+        client.getHomeTimeline(since_id, maxId, new JsonHttpResponseHandler() {
+            // SUCCESS
+
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.d("DEBUG:", response.toString());
+                // Clear tweetAdapter
+                if (maxId == Long.MAX_VALUE) {
+                    clearList();
+                }
+                // Add new data to tweetAdapter
+                addAll(Tweet.fromJSONArray(response));
+                // Now we call setRefreshing(false) to signal refresh has finished
+                stopRefreshing();
+            }
+
+            // FAILURE
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("DEBUG", errorResponse.toString());
+                stopRefreshing();
+//                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
     }
 }
