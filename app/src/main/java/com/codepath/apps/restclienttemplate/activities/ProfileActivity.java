92ac6f6032833d505ec52e8564fb97b0f1bd9f4f
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 13:22:44 +0530

    variables declared

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
@@ -1,7 +1,24 @@
 package com.codepath.apps.restclienttemplate.activities;
 
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.apps.restclienttemplate.models.User;
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
+
 /**
  * Created by ankit on 4/2/16.
  */
-public class ProfileActivity {
+public class ProfileActivity extends ActionBarActivity {
+
+    private User user;
+    private TwitterClient client;
+    private TextView tvUserName;
+    private TextView tvDescription;
+    private ImageView ivUserPhoto;
+    private TextView tvTweetCount;
+    private TextView tvFollowingCount;
+    private TextView tvFollowerCount;
+    private ImageView ivBackgroundImage;
 }
