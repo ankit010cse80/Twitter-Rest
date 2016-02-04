84bcc6030ad8ccef26dce1330b56362492aeb464
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 21:47:13 +0530

    extended TweetListFragment

--- app/src/main/java/com/codepath/apps/restclienttemplate/fragments/MentionsTimelineFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/MentionsTimelineFragment.java
@@ -3,5 +3,9 @@ package com.codepath.apps.restclienttemplate.fragments;
 /**
  * Created by ankit on 4/2/16.
  */
-public class MentionsTimelineFragment {
+public class MentionsTimelineFragment extends TweetsListFragment{
+    @Override
+    public void populateTimelineWithMaxId(Long sinceId, long maxId) {
+
+    }
 }
