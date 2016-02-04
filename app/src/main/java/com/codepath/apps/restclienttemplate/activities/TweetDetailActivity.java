cb4c749266fdc2c277f525a7795f95eb8915149d
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 16:27:18 +0530

    added onOptionsItemSelected method

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TweetDetailActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TweetDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Html;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -81,5 +82,20 @@ public class TweetDetailActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_tweet_detail, menu);
         return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
