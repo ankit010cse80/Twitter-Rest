6cab587d52ea8d6b647122348f8e3f99ce77b24c
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 21:49:47 +0530

    added new method getMentionsTimeLine ,

--- app/src/main/java/com/codepath/apps/restclienttemplate/fragments/MentionsTimelineFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/MentionsTimelineFragment.java
@@ -1,11 +1,19 @@
 package com.codepath.apps.restclienttemplate.fragments;
 
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
+
 /**
  * Created by ankit on 4/2/16.
  */
 public class MentionsTimelineFragment extends TweetsListFragment{
+
+    private TwitterClient client;
     @Override
     public void populateTimelineWithMaxId(Long sinceId, long maxId) {
 
+
     }
+
+
+
 }
