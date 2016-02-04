1659b91252848371263db51f932b4a5d67bdb374
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 15:23:43 +0530

    inflated layout

--- app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
@@ -1,10 +1,15 @@
 package com.codepath.apps.restclienttemplate.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.adapters.TweetListAdapter;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
@@ -28,6 +33,14 @@ public class TweetsListFragment extends Fragment {
         tweets = new ArrayList<Tweet>();
         // Create Adapter
         tweetAdapter = new TweetListAdapter(getActivity(), tweets);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tweets_list, container, false);
+
+
+        return v;
     }
 
 
