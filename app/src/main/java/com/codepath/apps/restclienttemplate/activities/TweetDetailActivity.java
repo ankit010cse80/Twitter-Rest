cc21f47e148e60a437cdf98dc6e372923396b268
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-04 15:52:47 +0530

    variables declaration

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TweetDetailActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TweetDetailActivity.java
@@ -1,7 +1,26 @@
 package com.codepath.apps.restclienttemplate.activities;
 
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.apps.restclienttemplate.TwitterApplication;
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
+
 /**
  * Created by ankit on 4/2/16.
  */
-public class TweetDetailActivity {
+public class TweetDetailActivity extends ActionBarActivity {
+
+    private ImageView ivUserPhoto;
+    private TextView tvUserName;
+    private TextView tvScreenName;
+    private TextView tvTweetDetail;
+    private TextView tvTimestamp;
+    private TextView tvTweetCount;
+    private TextView tvFavoritesCount;
+    private ImageButton btnReply;
+    private TwitterClient client = TwitterApplication.getRestClient();
+
 }
