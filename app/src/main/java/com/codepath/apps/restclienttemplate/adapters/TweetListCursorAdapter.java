8ef59963dc7c88009f6a372c2a464fd02f73c6aa
ankit010cse80 <ankit010cse080@gmail.com>
2016-02-05 11:27:16 +0530

    extending CursorAdapter

--- app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListCursorAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetListCursorAdapter.java
@@ -1,7 +1,27 @@
 package com.codepath.apps.restclienttemplate.adapters;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
 /**
  * Created by ankit on 5/2/16.
  */
-public class TweetListCursorAdapter {
+public class TweetListCursorAdapter extends CursorAdapter {
+
+    public TweetListCursorAdapter(Context context, Cursor c) {
+        super(context, c);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
 }
